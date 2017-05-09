eba81d767afbdb66d5f90f23209e876e5567b3d8
Aubrey Holland <aubreyholland@gmail.com>
2017-05-09 12:51:34 -0400

    basics for Sauce Labs test

--- config/nightwatch.js
+++ config/nightwatch.js
@@ -19,6 +19,25 @@ module.exports = {
   test_workers: false,
   test_settings: {
     'default': {
+      launch_url: 'http://ondemand.saucelabs.com:80',
+      filter: './test/**/*.e2e.spec.js',
+      selenium_host: 'ondemand.saucelabs.com',
+      selenium_port: 80,
+      username: 'aubreyadhoc',
+      access_key: '5b01dd5d-5faf-40ce-9c40-c0403fec76e7',
+      use_ssl: false,
+      silent: true,
+      output: true,
+      screenshots: {
+        enabled: false
+        // on_failure: true,
+        // path: 'logs/screenshots'
+      },
+      globals: {
+        waitForConditionTimeout: 10000
+      }
+    },
+    local: {
       launch_url: `localhost:${process.env.WEB_PORT || 3333}`,
       filter: './test/**/*.e2e.spec.js',
       selenium_host: 'localhost',
@@ -55,6 +74,58 @@ module.exports = {
         enabled: false,
         workers: parseInt(process.env.CONCURRENCY || 1, 10)
       },
+    },
+    chrome: { // your local Chrome browser (chromedriver)
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true
+      }
+    },
+    chromemac: { // browsers used on saucelabs:
+      desiredCapabilities: {
+        browserName: 'chrome',
+        platform: 'OS X 10.11',
+        version: '47'
+      }
+    },
+    ie11: {
+      desiredCapabilities: {
+        browserName: 'internet explorer',
+        platform: 'Windows 10',
+        version: '11.0'
+      }
+    },
+    firefox: {
+      desiredCapabilities: {
+        platform: 'XP',
+        browserName: 'firefox',
+        version: '33'
+      }
+    },
+    ie10: {
+      desiredCapabilities: {
+        platform: 'Windows 7',
+        browserName: 'internet explorer',
+        version: '10'
+      }
+    },
+    android_s4_emulator: {
+      desiredCapabilities: {
+        browserName: 'android',
+        deviceOrientation: 'portrait',
+        deviceName: 'Samsung Galaxy S4 Emulator',
+        version: '4.4'
+      }
+    },
+    iphone_6_simulator: {
+      desiredCapabilities: {
+        browserName: 'iPhone',
+        deviceOrientation: 'portrait',
+        deviceName: 'iPhone 6',
+        platform: 'OSX 10.10',
+        version: '8.4'
+      }
     },
     accessibility: {
       filter: './test/accessibility/*.spec.js',
