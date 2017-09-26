fe77123df2cc42998f3ce275768360cc4c640751
Eugene Doan <eugene@adhocteam.us>
2017-09-26 10:02:58 -0400

    Space between hours.

--- src/js/rx/containers/Active.jsx
+++ src/js/rx/containers/Active.jsx
@@ -141,7 +141,7 @@ class Active extends React.Component {
           It looks like you don’t have any VA prescriptions to refill right now.
           If you think this is a mistake, please contact your VA health care team and ask them to check your prescription records.
           If you need more help, please call the Vets.gov Help Desk at 1-855-571-7286 (TTY: 1-800-829-4833).
-          We’re here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET).
+          We’re here Monday - Friday, 8:00 a.m. - 8:00 p.m. (ET).
         </p>
       );
     }
