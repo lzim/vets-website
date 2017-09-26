2878adf33b69751a75037fd12fdfb048110e1b36
Eugene Doan <eugene@adhocteam.us>
2017-09-26 12:06:32 -0400

    Check if prescriptions are available on mount and show alert if they are not.

--- src/js/rx/containers/Main.jsx
+++ src/js/rx/containers/Main.jsx
@@ -2,15 +2,51 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import AlertBox from '../../common/components/AlertBox';
-import { closeDisclaimer } from '../actions/disclaimer';
+import LoadingIndicator from '../../common/components/LoadingIndicator';
+
 import { closeAlert } from '../actions/alert.js';
+import { closeDisclaimer } from '../actions/disclaimer';
+import { loadPrescriptions } from '../actions/prescriptions';
 import Disclaimer from '../components/Disclaimer';
 import ErrorView from '../components/ErrorView';
 import SettingsButton from '../components/SettingsButton';
 import TabNav from '../components/TabNav';
 
 class Main extends React.Component {
+  componentDidMount() {
+    this.props.loadPrescriptions();
+  }
+
   render() {
+    const { loading, prescriptions } = this.props;
+
+    if (loading) {
+      return <LoadingIndicator message="Loading your prescriptions..."/>;
+    }
+
+    if (!prescriptions) {
+      const content = (
+        <div>
+          <h4>No active VA prescriptions </h4>
+          <div>
+            It looks like you don't have any active VA prescriptions. If you're taking a medicine that you don't see listed here—or if you have any questions about your current medicines—please contact your VA health care team. If you need more help, please call the Vets.gov Help Desk at 1-855-574-7286 (TTY: 1-800-829-4833). We’re here Monday - Friday, 8:00 a.m. - 8:00 p.m. (ET).
+          </div>
+        </div>
+      );
+
+      return (
+        <div>
+          <div className="rx-app-title">
+            <h1>Prescription Refill</h1>
+          </div>
+          <AlertBox
+            content={content}
+            isVisible
+            status="warning"/>
+        </div>
+      );
+    }
+
     return (
       <ErrorView errors={this.props.errors}>
         <AlertBox
@@ -35,16 +71,21 @@ class Main extends React.Component {
 
 const mapStateToProps = (state) => {
   const rxState = state.health.rx;
+  const { alert, disclaimer, errors, prescriptions } = rxState;
+  const { items, loading } = prescriptions.history;
   return {
-    alert: rxState.alert,
-    disclaimer: rxState.disclaimer,
-    errors: rxState.errors.errors,
+    alert,
+    disclaimer,
+    errors,
+    loading,
+    prescriptions: items
   };
 };
 
 const mapDispatchToProps = {
   closeAlert,
   closeDisclaimer,
+  loadPrescriptions,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
